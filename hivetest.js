70c278f46d1cafae4a7998b90bbd487004c6dde3
Denis Spasyuk <34203011+dspasyuk@users.noreply.github.com>
2024-09-07 23:55:07 -0600

    Add files via upload

--- hivetest.js
+++ hivetest.js
@@ -1,16 +1,16 @@
-const Hive  = require('./hive.js');
+import Hive from './hive.js';
 
 async function test() {
-    const pathToDocs = './docs';
-    await Hive.init('Documents', './db/Documents/db.json', pathToDocs);
-    //or 
-    // await Hive.init('Documents');
+    await Hive.init('Documents', './db/Documents/db.json',"./docs");
+    // await db.initTransformers();
+    // db.loadToMemory();  
     const vector = await Hive.getVector("Human Factors Workscope", Hive.TransOptions);
-    console.time("vector");
-    results = await Hive.find('Documents', vector.data, 10);
-    console.timeEnd("vector");
-    // console.log(results);
-    results.forEach((r) => console.log(r.document.meta))
+    for (let i = 0; i < 10; i++) {
+        console.time("vector");
+        var results = await Hive.find(vector.data, 10);
+        console.timeEnd("vector");    
+    }
+   
 }
 test()
 
