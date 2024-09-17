ceb10d0e92e5bc0e7707a4f7560f6d2109aa2067
Denis Spasyuk <34203011+dspasyuk@users.noreply.github.com>
2024-09-17 17:02:39 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 import Hive from './hive.js';
 
 await Hive.init('Documents', './db/Documents/db.json',"./docs");
-const vector = await Hive.getVector("Human Factors Workscope", Hive.TransOptions);
+const vector = await Hive.getVector("Human Factors Workscope", Hive.transformersOptions);
 var results = await Hive.find(vector.data, 10);
+results.forEach(element => {
+     console.log(element.document.meta, element.similarity);
+});
