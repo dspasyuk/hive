69fa17a55ab49e4e67eb2960fc0aec4ef355a307
Denis Spasyuk <34203011+dspasyuk@users.noreply.github.com>
2024-09-18 12:03:35 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-import Hive from './hive.js';
+import Hive from 'deonis/hive';
+await Hive.init('Documents', "./DB/Documents/db.js", "./doc", );
 
-await Hive.init('Documents', './db/Documents/db.json',"./docs");
-const vector = await Hive.getVector("Human Factors Workscope", Hive.transformersOptions);
-var results = await Hive.find(vector.data, 10);
+const vector = await Hive.getVector("Hello World", Hive.TransOptions);
+const imgvector = await Hive.getVector("path/to/test/image.jpg", Hive.TransOptions);
+var results = await Hive.find(imgvector.data, 3);
 results.forEach(element => {
      console.log(element.document.meta, element.similarity);
+});
+
+results = await Hive.find(vector.data, 3);
+results.forEach(element => {
+    console.log(element.document.meta);
 });
