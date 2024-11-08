22df3229ed3dcf81d174a6406faa52ddeee25a62
Denis Spasyuk <34203011+dspasyuk@users.noreply.github.com>
2024-11-07 20:30:07 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-import Hive from 'deonis/hive';
-await Hive.init('Documents', "./DB/Documents/db.js", "./doc", );
+import Hive from '@deonis/hive';
 
-const vector = await Hive.getVector("Hello World", Hive.TransOptions);
-const imgvector = await Hive.getVector("path/to/test/image.jpg", Hive.TransOptions);
+const options = {
+    dbName: "Documents",
+    pathToDB: "./db/Documents/db.json",
+    pathToDocs: "./docs",
+    type: "text",
+    documents: {
+      text: [".txt", ".doc", ".docx", ".pdf"],
+      image: [".png", ".jpg", ".jpeg"]
+    }
+  };
+await Hive.init(options); 
+const vector = await Hive.getVector("Tut, I have lost myself; I am not here; This is not Romeo, he's some other where.", Hive.TransOptions);
+const imgvector = await Hive.getVector("./docs/Smithsonian/smithm352e60b53736d45668efde4e22339f424c32c.jpg", Hive.TransOptions);
 var results = await Hive.find(imgvector.data, 3);
 results.forEach(element => {
      console.log(element.document.meta, element.similarity);
@@ -10,5 +20,5 @@ results.forEach(element => {
 
 results = await Hive.find(vector.data, 3);
 results.forEach(element => {
-    console.log(element.document.meta);
+    console.log(element.document.meta, element.similarity);
 });
